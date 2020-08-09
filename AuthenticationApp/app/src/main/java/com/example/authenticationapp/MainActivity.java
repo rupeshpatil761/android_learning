35ce4029a09f03741d2b70f87c1ff001f1ed998d
Rupesh Patil <rupeshpatil.461@gmail.com>
2020-08-09 17:01:47 +0530

    Adding firebase authentication test project

---
+++ AuthenticationApp/app/src/main/java/com/example/authenticationapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.authenticationapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.authenticationapp.login.LoginActivity;
+import com.example.authenticationapp.register.RegisterActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button registerBtn, loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initializeViews() {
+        registerBtn = findViewById(R.id.register);
+        loginBtn = findViewById(R.id.login);
+    }
+}
